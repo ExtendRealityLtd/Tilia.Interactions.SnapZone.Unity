0a5f63186a93c891a7456bbfa1b5ffa3c2146130
extendreality <hello@extendreality.io>
2020-03-09 22:29:30 +0000

    feat(structure): port snapzone prefab
    The SnapZone prefab from VRTK.Prefabs has been ported over to
    its own Tilia repo.

---
+++ Runtime/SharedResources/Scripts/SnapZoneActivator.cs
@@ -0,0 +1,35 @@
+﻿namespace Tilia.Interactions.SnapZone
+{
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.XmlDocumentationAttribute;
+    using UnityEngine;
+    using Zinnia.Data.Attribute;
+    using Zinnia.Tracking.Collision;
+
+    /// <summary>
+    /// Holds the linked settings for a Interactions SnapZone.
+    /// </summary>
+    public class SnapZoneActivator : MonoBehaviour
+    {
+        #region Facade Settings
+        /// <summary>
+        /// The public interface facade.
+        /// </summary>
+        [Serialized]
+        [field: Header("Facade Settings"), DocumentedByXml, Restricted]
+        public SnapZoneFacade Facade { get; protected set; }
+        /// <summary>
+        /// The <see cref="CollisionTracker"/> used for tracking collisions on the zone.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml, Restricted]
+        public CollisionTracker CollisionTracker { get; protected set; }
+        /// <summary>
+        /// The <see cref="Collider"/> that is used to determine the collidable area for the zone.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml]
+        public Collider Collider { get; set; }
+        #endregion
+    }
+}
